02289ab0f72e699f96142921e353365327b08125
Kevin Hermelin <kevin.hermelin@outlook.com>
2025-09-02 23:00:41 +0000

    feat: implement scroll

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod file_buffer;
 
 use clap::{Parser, command};
 use color_eyre::Result;
-use crossterm::event::{self, Event};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
@@ -28,7 +28,10 @@ fn main() -> Result<()> {
     let file_path = cli.file_path;
     let buffer = FileBuffer::from_file(&file_path).expect("should be able to read file");
 
-    let mut app = App { buffer };
+    let mut app = App {
+        buffer,
+        scroll_offset_line: 0,
+    };
 
     color_eyre::install()?;
     let mut terminal = ratatui::init();
@@ -39,26 +42,63 @@ fn main() -> Result<()> {
 
 pub struct App {
     buffer: FileBuffer,
+    scroll_offset_line: i16,
+}
+
+enum Action {
+    ScrollUp,
+    ScrollDown,
+    None,
 }
 
 impl App {
     fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
         loop {
             terminal.draw(|frame| self.draw(frame))?;
-            if matches!(event::read()?, Event::Key(_)) {
-                break Ok(());
+            self.handle_event()?;
+        }
+    }
+    fn handle_event(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
             }
+            _ => {}
+        };
+        Ok(())
+    }
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        let action = match key_event.code {
+            KeyCode::Down => Action::ScrollDown,
+            KeyCode::Up => Action::ScrollUp,
+            _ => Action::None,
+        };
+        self.handle_action(action);
+    }
+    fn handle_action(&mut self, action: Action) {
+        match action {
+            Action::ScrollDown => self.move_scroll(1),
+            Action::ScrollUp => self.move_scroll(-1),
+            Action::None => {}
         }
     }
     fn draw(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
+    }
+    fn move_scroll(&mut self, lines: i16) {
+        let number_of_lines: i16 = self.buffer.content.lines().count().try_into().unwrap();
+
+        self.scroll_offset_line = (self.scroll_offset_line + lines).clamp(0, number_of_lines - 1);
     }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from(self.buffer.file_path.display().to_string().bold());
-        let instructions = Line::from(vec![" Quit ".into(), "<Any Key> ".blue().bold()]);
+        let instructions = Line::from(vec![
+            " Scroll Up/Down ".into(),
+            "<Up>/<Down> ".blue().bold(),
+        ]);
         let block = Block::bordered()
             .title(title.centered())
             .title_bottom(instructions.centered())
@@ -66,7 +106,10 @@ impl Widget for &App {
 
         let buffer_text = Text::from(self.buffer.content.clone());
 
-        Paragraph::new(buffer_text).block(block).render(area, buf);
+        Paragraph::new(buffer_text)
+            .block(block)
+            .scroll((self.scroll_offset_line.try_into().unwrap(), 0))
+            .render(area, buf);
     }
 }
 
@@ -81,13 +124,40 @@ mod tests {
         let app = App {
             buffer: FileBuffer {
                 file_path: PathBuf::from("example.txt"),
-                content: String::from("this is a test file"),
+                content: String::from(
+                    "this should not be visible.\nthis is a test file\nspanning multiple\nlines",
+                ),
             },
+            scroll_offset_line: 1,
         };
         let mut terminal = Terminal::new(TestBackend::new(80, 20)).unwrap();
         terminal
             .draw(|frame| frame.render_widget(&app, frame.area()))
             .unwrap();
         assert_snapshot!(terminal.backend());
+    }
+
+    #[test]
+    fn test_handle_scroll_action() {
+        let mut app = App {
+            buffer: FileBuffer {
+                file_path: PathBuf::from("example.txt"),
+                content: String::from("this is a test file\nspanning multiple\nlines"),
+            },
+            scroll_offset_line: 0,
+        };
+
+        // Can not scroll up when on first line.
+        app.handle_action(Action::ScrollUp);
+        assert_eq!(app.scroll_offset_line, 0);
+        app.handle_action(Action::ScrollDown);
+        assert_eq!(app.scroll_offset_line, 1);
+        app.handle_action(Action::ScrollDown);
+        assert_eq!(app.scroll_offset_line, 2);
+        // Scrolling beyond last line should not be possible.
+        app.handle_action(Action::ScrollDown);
+        assert_eq!(app.scroll_offset_line, 2);
+        app.handle_action(Action::ScrollUp);
+        assert_eq!(app.scroll_offset_line, 1);
     }
 }
