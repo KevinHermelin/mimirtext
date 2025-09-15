095639c443c4bb4457b179f1fdb097c32d0b4486
Kevin Hermelin <kevin.hermelin@outlook.com>
2025-09-15 14:32:04 +0000

    build: export package in flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,21 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+
+        naersk' = pkgs.callPackage naersk {};
+      in rec {
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [ pkgs.rustc pkgs.cargo pkgs.rustfmt pkgs.cargo-insta ];
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
