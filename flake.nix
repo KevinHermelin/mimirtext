13cabf32d580abb8d12188334f10e3b3dfb81c57
Kevin Hermelin <kevin.hermelin@outlook.com>
2026-04-25 10:08:27 +0000

    build: clean up nix flake and add test package
    This removes "defaultPackage" from earlier and replaces it with "packages.default". "packages.test" is added.

--- flake.nix
+++ flake.nix
@@ -5,20 +5,42 @@
     naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      naersk,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
 
-        naersk' = pkgs.callPackage naersk {};
-      in rec {
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
+        naersk' = pkgs.callPackage naersk { };
+      in
+      {
+        packages = {
+          default = naersk'.buildPackage {
+            src = ./.;
+          };
+
+          test = naersk'.buildPackage {
+            src = ./.;
+            mode = "test";
+          };
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.rustc pkgs.cargo pkgs.rustfmt pkgs.cargo-insta pkgs.clippy ];
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rustfmt
+            pkgs.cargo-insta
+            pkgs.clippy
+          ];
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
-      });
+      }
+    );
 }
